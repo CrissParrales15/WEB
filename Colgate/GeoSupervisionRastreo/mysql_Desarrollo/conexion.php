a5a7b2af905b21e42c93881e2c337466a27d3f43
CrissParrales15 <cparrales@xplora.net>
2026-09-08 11:17:01 -0500

    colgate
    update

--- Colgate/GeoSupervisionRastreo/mysql_Desarrollo/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "coordenadas");
-
-  if ($conn->connect_error) {
-    die("ERROR: No se puede conectar al servidor: " . $conn->connect_error);
-  }
